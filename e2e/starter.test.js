e28bb65c363d9ef56ce855a10565b0b86fbd1b10
Maschga <delphi@freenet.de>
2025-09-19 12:56:32 +0200

    wip

---
+++ e2e/starter.test.js
@@ -0,0 +1,9 @@
+describe("Example", () => {
+  beforeAll(async () => {
+    await device.launchApp({ newInstance: true });
+  });
+
+  beforeEach(async () => {});
+
+  it("should test something", async () => {});
+});
