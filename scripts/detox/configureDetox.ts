078d9b38dada5936456670974e90e72040856ca7
Maschga <delphi@freenet.de>
2025-09-21 19:04:58 +0200

    add detox plugin

---
+++ scripts/detox/configureDetox.ts
@@ -0,0 +1,18 @@
+import { ConfigPlugin, withPlugins } from "expo/config-plugins";
+import patchAppGradle from "./src/patchBuildGradles/patchAppGradle";
+import patchProjectGradle from "./src/patchBuildGradles/patchProjectGradle";
+import addAndroidTest from "./src/addAndroidTest";
+import enableUnencryptedTraffic from "./src/enableUnencryptedTraffic";
+
+const configureDetox: ConfigPlugin = (config) => {
+  console.log("RUNNNNNNNNNNNNNNNNNNNNNNNNNNNINNNNNNNNNG");
+
+  return withPlugins(config, [
+    patchProjectGradle,
+    patchAppGradle,
+    addAndroidTest,
+    enableUnencryptedTraffic,
+  ]);
+};
+
+export default configureDetox;
---
+++ scripts/detox/src/patchBuildGradles/patchAppGradle.ts
@@ -0,0 +1,42 @@
+import { ConfigPlugin, withAppBuildGradle } from "expo/config-plugins";
+
+const patchAppGradle: ConfigPlugin = (config) => {
+  return withAppBuildGradle(config, (config) => {
+    for (const f of [
+      addDetoxDefaultConfigBlock,
+      addDetoxProguardRules,
+      setGradleAndroidTestImplementation,
+    ]) {
+      config.modResults.contents = f(config.modResults.contents);
+    }
+
+    return config;
+  });
+};
+
+const addDetoxDefaultConfigBlock = (config: string) => {
+  return config.replace(
+    "defaultConfig {",
+    `defaultConfig {
+        // detox-plugin-default-config
+        testBuildType System.getProperty('testBuildType', 'debug')
+        testInstrumentationRunner 'androidx.test.runner.AndroidJUnitRunner'`,
+  );
+};
+
+const addDetoxProguardRules = (config: string) => {
+  return config.replace(
+    `proguardFiles getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro"`,
+    `proguardFile "\${rootProject.projectDir}/../node_modules/detox/android/detox/proguard-rules-app.pro"`,
+  );
+};
+
+const setGradleAndroidTestImplementation = (config: string) => {
+  return config.replace(
+    "dependencies {",
+    `androidTestImplementation('com.wix:detox:+')
+    implementation 'androidx.appcompat:appcompat:1.1.0'`,
+  );
+};
+
+export default patchAppGradle;
